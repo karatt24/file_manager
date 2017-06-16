91ee08f767d16513b3219da1dc8eb4a353642ae5
karatt24 <karatt1987@yandex.ru>
2017-06-16 12:43:57 +0600

    Dvizheniya cursora dodelat'

--- fc.c
+++ fc.c
@@ -12,12 +12,40 @@ void sig_winch(int signo){
         resizeterm(size.ws_row,size.ws_col);
 }
 
+key_move(WINDOW *wnd, int ch, int sh){
+	int x,y;
+/*	if (ch == KEY_TAB)
+		if (wnd == wnd1){
+			wnd=wnd2;
+			wmove(wnd, 1,1);
+		}
+		else{
+			wnd=wnd1;
+			wmove(wnd, 1,1);
+		}
+*/	switch(ch){
+		case KEY_UP:
+			getyx(wnd, y, x);
+			if (y > 0){
+				y--;
+				wmove(wnd, y, x);
+			}
+		case KEY_DOWN:
+			getyx(wnd, y, x);
+			if(y < sh){
+				y++;
+				wmove(wnd, y, x);
+			}
+	}
+}
 
 
 int main(){
 
-	WINDOW *wnd1,*wnd2;
+	WINDOW *win,*wnd1,*wnd2;
 	DIR *dp;
+	struct  dirent *dir;
+	int x,y, ch, sh=0;
 	dp=opendir(".");
 	initscr();
 	signal(SIGWINCH, sig_winch);
@@ -25,16 +53,52 @@ int main(){
 	start_color();
         refresh();
         init_pair(1, COLOR_YELLOW, COLOR_BLUE);
-        wnd1 = newwin(36,70,1,1);
-	wnd2 = newwin(36,70,1,72);
+        wnd1 = newwin(25,40,1,1);
+	wnd2 = newwin(25,40,1,42);
 	box(wnd1,'|','-');
 	box(wnd2,'|','-');
-	wattron(wnd1, COLOR_PAIR(1));
-	wattron(wnd2, COLOR_PAIR(1));
+	wbkgd(wnd1, COLOR_PAIR(1));
+	wbkgd(wnd2, COLOR_PAIR(1));
 	wrefresh(wnd1);
 	wrefresh(wnd2);
-	getch();
+	wmove(wnd1, 1,1);
+	while((dir=readdir(dp)) != NULL){
+		wprintw(wnd1,"%s\n",dir->d_name);
+		getyx(wnd1,y,x);
+		wmove(wnd2,y-1,x+1);
+		wprintw(wnd2,"%s\n",dir->d_name);
+                wmove(wnd1, y,x+1);
+		sh++;
+	}
+	wrefresh(wnd2);
 
+	wmove(wnd1, 1, 1);
+	wrefresh(wnd1);
+	win=wnd1;
+	keypad(win, TRUE);
+	while(1){
+
+		ch = wgetch(win);
+		if (ch == 9)
+	                if (win == wnd1){
+	                        win=wnd2;
+	                        wmove(win, 1,1);
+				keypad(win, TRUE);
+	                }
+        	        else{
+                	        win=wnd1;
+                	        wmove(win, 1,1);
+				keypad(win, TRUE);
+                	}
+
+		key_move(win, ch, sh);
+		if (ch == 27)
+			break;
+
+	}
+
+//	getch();
+	system("clear");
 	endwin();
 
 }
