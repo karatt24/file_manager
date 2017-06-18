6fd24c6165384bf96a610ee040b1d3d8c90a46fe
karatt1987 <karatt1987@yandex.ru>
2017-06-19 01:48:17 +0700

    Realisoval navigaciu

--- fc.c
+++ fc.c
@@ -12,30 +12,68 @@ void sig_winch(int signo){
         resizeterm(size.ws_row,size.ws_col);
 }
 
-key_move(WINDOW *wnd, int ch, int sh){
+int screen(DIR *dp, WINDOW *wnd1, WINDOW *wnd2){
+        int y,x,i=0;
+        DIR *dpp=opendir(".");
+        struct  dirent *dir;
+        wclear(wnd1);
+        wclear(wnd2);
+        box(wnd1,'|','-');
+        box(wnd2,'|','-');
+        wmove(wnd1,1,1);
+        while((dir=readdir(dpp)) != NULL){
+                wprintw(wnd1,"%s\n",dir->d_name);
+                getyx(wnd1,y,x);
+                wmove(wnd2,y-1,x+1);
+                wprintw(wnd2,"%s\n",dir->d_name);
+                wmove(wnd1, y,x+1);
+                i++;
+        }
+        wrefresh(wnd1);
+        wrefresh(wnd2);
+        closedir(dpp);
+        return i;
+}
+/*
+int key_tab(DIR *dp, WINDOW *win,WINDOW *wnd1, WINDOW *wnd2, int ch){
+	int sh;
+	if (ch == 9)
+                if (win == wnd1){
+                        win=wnd2;
+                        sh=screen(dp, wnd1, wnd2);
+                        wmove(win, 1,1);
+                        keypad(win, TRUE);
+                }
+                else{
+                        win=wnd1;
+                        sh=screen(dp, wnd1, wnd2);
+                        wmove(win, 1,1);
+                        keypad(win, TRUE);
+                }
+	return sh;
+}
+*/
+void key_move(WINDOW *wnd, int ch, int sh){
 	int x,y;
-/*	if (ch == KEY_TAB)
-		if (wnd == wnd1){
-			wnd=wnd2;
-			wmove(wnd, 1,1);
-		}
-		else{
-			wnd=wnd1;
-			wmove(wnd, 1,1);
-		}
-*/	switch(ch){
+	switch(ch){
 		case KEY_UP:
 			getyx(wnd, y, x);
 			if (y > 0){
 				y--;
 				wmove(wnd, y, x);
 			}
+			break;
 		case KEY_DOWN:
 			getyx(wnd, y, x);
 			if(y < sh){
 				y++;
 				wmove(wnd, y, x);
 			}
+			break;
+		case 10:
+			getyx(wnd, y,x);
+			wprintw(wnd, "WAAAAW");
+			break;
 	}
 }
 
@@ -62,16 +100,17 @@ int main(){
 	wrefresh(wnd1);
 	wrefresh(wnd2);
 	wmove(wnd1, 1,1);
-	while((dir=readdir(dp)) != NULL){
+/*	while((dir=readdir(dp)) != NULL){
 		wprintw(wnd1,"%s\n",dir->d_name);
 		getyx(wnd1,y,x);
 		wmove(wnd2,y-1,x+1);
 		wprintw(wnd2,"%s\n",dir->d_name);
                 wmove(wnd1, y,x+1);
 		sh++;
-	}
-	wrefresh(wnd2);
+	}*/
+//	wrefresh(wnd2);
 
+	sh=screen(dp, wnd1, wnd2);
 	wmove(wnd1, 1, 1);
 	wrefresh(wnd1);
 	win=wnd1;
@@ -79,22 +118,30 @@ int main(){
 	while(1){
 
 		ch = wgetch(win);
+//		sh=key_tab(dp, win, wnd1, wnd2, ch);
 		if (ch == 9)
 	                if (win == wnd1){
 	                        win=wnd2;
+				sh=screen(dp, wnd1, wnd2);
 	                        wmove(win, 1,1);
 				keypad(win, TRUE);
 	                }
         	        else{
                 	        win=wnd1;
+				sh=screen(dp, wnd1, wnd2);
                 	        wmove(win, 1,1);
 				keypad(win, TRUE);
                 	}
 
 		key_move(win, ch, sh);
-		if (ch == 27)
-			break;
-
+		wrefresh(win);
+		if (ch == 27){
+//			system("clear");
+			delwin(wnd1);
+			delwin(wnd2);
+			endwin();
+			exit(EXIT_SUCCESS);
+		}
 	}
 
 //	getch();
